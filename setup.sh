8b7dab163ec3a33f3816c221c0a5b95bc0360047
Vatsal Patel <68418350+Vatsal2251@users.noreply.github.com>
2022-03-30 16:55:21 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,3 @@ echo "
 [general]n
 email = "your-email@domain.com"n
 " > ~/.streamlit/credentials.toml
-echo "
-[server]n
-headless = truen
-enableCORS=falsen
-port = $PORTn
-" > ~/.streamlit/config.toml
